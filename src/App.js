b4f38013f180ce6163813e4bf8fed56ba4a6a3c8
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2023-08-25 12:43:47 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was created by Adriana Cividini and is{" "}
-        <a
-          href="https://github.com/AdrianaCividini/weather-app-react/settings"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was created by Adriana Cividini and is{" "}
+          <a
+            href="https://github.com/AdrianaCividini/weather-app-react/settings"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
