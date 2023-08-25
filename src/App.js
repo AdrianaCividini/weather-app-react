8c05c3b30398169ce72c07d94eec0f542fbc512d
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2023-08-25 12:15:59 +0200

    Added the github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was created by Adriana Cividini and is{" "}
+        <a
+          href="https://github.com/AdrianaCividini/weather-app-react/settings"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
