b14ef088eb92c93e854f7c265e11e4ac53cb9ff7
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2023-08-29 11:10:58 +0200

    Added the API integration.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <p>Weather App</p>
+        <Weather defaultCity="Rio de janeiro" />
         <footer>
           This project was created by Adriana Cividini and is{" "}
           <a
